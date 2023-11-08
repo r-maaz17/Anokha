c60357e713eb1eca7847bde5db0fb6724fe21c8b
Amar Zaidi <amarzaidi12321@gmail.com>
2023-11-08 15:38:32 +0500

    Update SignUp.js

--- Frontend/frontend/src/components/SignUp.js
+++ Frontend/frontend/src/components/SignUp.js
@@ -1,7 +1,35 @@
 import React from "react";
-import { Button } from "antd";
+import {
+    MDBContainer,
+    MDBInput,
+    MDBCheckbox,
+    MDBBtn,
+    MDBIcon
+} from 'mdb-react-ui-kit';
+import Navbar from "./Navbar";
+
 const SignUp = () => {
-<Button type="primary" size='large'>SignUp</Button>
+return (
+    <div>
+    <Navbar></Navbar>
+    <MDBContainer className="p-3 my-5 d-flex flex-column w-50">
+  
+            <MDBInput wrapperClass='mb-4' label='username' id='form3' type='username' />
+            <MDBInput wrapperClass='mb-4' label='Email address' id='form1' type='email' />
+            <MDBInput wrapperClass='mb-4' label='Password' id='form2' type='password' />
+        
+
+            <MDBBtn className="mb-4">Sign Up</MDBBtn>
+
+            <div className="text-center">
+                <p>Already a Member? <a href="#!">Sign In</a></p>
+
+
+            </div>
+
+        </MDBContainer>
+        </div>
+);
 }
 
 export default SignUp;
