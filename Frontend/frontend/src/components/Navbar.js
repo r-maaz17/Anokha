1e363baa4fa7eedb79ed9fc55077c06a9f748c9b
Amar Zaidi <amarzaidi12321@gmail.com>
2023-11-08 15:37:22 +0500

    Update Navbar
    Navbar is updated

--- Frontend/frontend/src/components/Navbar.js
+++ Frontend/frontend/src/components/Navbar.js
@@ -2,11 +2,19 @@ import React from 'react';
 import { Layout, Menu, Button } from 'antd';
 import SignIn from './SignIn';
 import SignUp from './SignUp';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 const { Header } = Layout;
 
 function Navbar() {
+  const navigate = useNavigate();
+
+  const handleClick = (path) => {
+    // Use the navigate function to go to a different route
+    navigate(path);
+  };
+  
+
   return (
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
       <div class="container-fluid">
@@ -17,21 +25,22 @@ function Navbar() {
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav me-auto mb-2 mb-lg-0">
             <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#">Home</a>
+              <Link class="nav-link active" aria-current="page" to="/">Home</Link>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="#">Link</a>
             </li>
+            <form class="d-flex" role="search">
+              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+              <button class="btn btn-outline-success" type="submit">Search</button>
+            </form>
           </ul>
-          <form class="d-flex" role="search">
-            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-            <button class="btn btn-outline-success" type="submit">Search</button>
-          </form>
+
           <li class="nav-item">
-            <Button className="btn btn-primary">Sign Up</Button>
+            <Button className="btn btn-primary" onClick={() => handleClick('/signup')}>Sign Up</Button>
           </li>
           <li class="nav-item">
-            <Button className="btn btn-primary">Sign in</Button>
+            <Button className="btn btn-primary" onClick={() => handleClick('/signin')}>Sign in</Button>
           </li>
         </div>
       </div>
