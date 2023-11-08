3e908c27d374473e32c158365a635219576649ea
Amar Zaidi <amarzaidi12321@gmail.com>
2023-11-08 15:37:39 +0500

    Update SignIn.js
    SignIn component is updated

--- Frontend/frontend/src/components/SignIn.js
+++ Frontend/frontend/src/components/SignIn.js
@@ -7,28 +7,34 @@ import {
     MDBIcon
 }
     from 'mdb-react-ui-kit';
+import Navbar from './Navbar';
 
 const SignIn = () => {
-    <MDBContainer className="p-3 my-5 d-flex flex-column w-50">
+    return (
+        <div>
+        <Navbar/>
+        <MDBContainer className="p-3 my-5 d-flex flex-column w-50">
 
-        <MDBInput wrapperClass='mb-4' label='Email address' id='form1' type='email' />
-        <MDBInput wrapperClass='mb-4' label='Password' id='form2' type='password' />
+            <MDBInput wrapperClass='mb-4' label='Email address' id='form1' type='email' />
+            <MDBInput wrapperClass='mb-4' label='Password' id='form2' type='password' />
 
-        <div className="d-flex justify-content-between mx-3 mb-4">
-            <MDBCheckbox name='flexCheck' value='' id='flexCheckDefault' label='Remember me' />
-            <a href="!#">Forgot password?</a>
+            <div className="d-flex justify-content-between mx-3 mb-4">
+                <MDBCheckbox name='flexCheck' value='' id='flexCheckDefault' label='Remember me' />
+                <a href="!#">Forgot password?</a>
+            </div>
+
+            <MDBBtn className="mb-4">Sign in</MDBBtn>
+
+            <div className="text-center">
+                <p>Not a member? <a href="#!">Register</a></p>
+                <p>or sign up with:</p>
+
+
+            </div>
+
+        </MDBContainer>
         </div>
-
-        <MDBBtn className="mb-4">Sign in</MDBBtn>
-
-        <div className="text-center">
-            <p>Not a member? <a href="#!">Register</a></p>
-            <p>or sign up with:</p>
-
-        </div>
-
-    </MDBContainer>
-
+    );
 }
 
 export default SignIn;
