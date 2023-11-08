519adf42f71464a00924b64c75587845d43cafc4
Amar Zaidi <amarzaidi12321@gmail.com>
2023-11-08 13:56:44 +0500

    Create ordersController.js

---
+++ Backend/controller/ordersController.js
@@ -0,0 +1,65 @@
+const Order = require('../models/orders');
+
+// Create a new order
+exports.createOrder = async (req, res) => {
+  try {
+    const order = new Order(req.body);
+    await order.save();
+    res.status(201).json(order);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+// Get all orders
+exports.getOrders = async (req, res) => {
+  try {
+    const orders = await Order.find();
+    res.status(200).json(orders);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+// Get an order by OrderId
+exports.getOrderById = async (req, res) => {
+  try {
+    const order = await Order.findOne({ OrderId: req.params.orderId });
+    if (!order) {
+      return res.status(404).json({ message: 'Order not found' });
+    }
+    res.status(200).json(order);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+// Update an order by OrderId
+exports.updateOrder = async (req, res) => {
+  try {
+    const order = await Order.findOneAndUpdate(
+      { OrderId: req.params.orderId },
+      req.body,
+      { new: true }
+    );
+    if (!order) {
+      return res.status(404).json({ message: 'Order not found' });
+    }
+    res.status(200).json(order);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+// Delete an order by OrderId
+exports.deleteOrder = async (req, res) => {
+  try {
+    const order = await Order.findOneAndRemove({ OrderId: req.params.orderId });
+    if (!order) {
+      return res.status(404).json({ message: 'Order not found' });
+    }
+    res.status(204).send();
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
