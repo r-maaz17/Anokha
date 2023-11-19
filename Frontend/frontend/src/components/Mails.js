4e1537cd6c6bcb2e3da1f684b6f1ce773d12a88a
Amar Zaidi <amarzaidi12321@gmail.com>
2023-11-19 23:41:05 +0500

    Create Mails.js
    Mails to show mails

---
+++ Frontend/frontend/src/components/Mails.js
@@ -0,0 +1,12 @@
+import React, { startTransition } from 'react'
+import { Link } from 'react-router-dom'
+import { Rating } from 'react-simple-star-rating'
+
+
+export const Mails = (props) => {
+    return (
+        <div>
+            mailbox
+        </div>
+    );
+}
