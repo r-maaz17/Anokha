45d94f1a6b87b8de50635bc4308145daf3a74acc
Amar Zaidi <amarzaidi12321@gmail.com>
2023-11-19 23:40:53 +0500

    Create Feedbacks.js
    Feedbacks page is to show the all feedbacks of the system given by public

---
+++ Frontend/frontend/src/components/Feedbacks.js
@@ -0,0 +1,12 @@
+import React, { startTransition } from 'react'
+import { Link } from 'react-router-dom'
+import { Rating } from 'react-simple-star-rating'
+
+
+export const Feedbacks = (props) => {
+    return (
+        <div>
+            Feedbacks
+        </div>
+    );
+}
