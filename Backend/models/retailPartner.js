ad6ee2229865a63aa9c3bd80b6f44c67456b0f69
Amar Zaidi <amarzaidi12321@gmail.com>
2023-11-22 19:39:43 +0500

    Remove Unnecessary Files
    These are some unnecessary files that client do not want to continue with it.

--- Backend/models/retailPartner.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const retailPartnerSchema = new mongoose.Schema({
-  PartnerId: Number,
-  PartnerName: String,
-  OutletLocations: [String], // Assuming outlet locations are stored as an array of strings
-});
-
-module.exports = mongoose.model('RetailPartner', retailPartnerSchema);
--- Backend/models/supplier.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const supplierSchema = new mongoose.Schema({
-  SupplierId: Number,
-  SupplierName: String,
-  ContactInfo: String,
-});
-
-module.exports = mongoose.model('Supplier', supplierSchema);
