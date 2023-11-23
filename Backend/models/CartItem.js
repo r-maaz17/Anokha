8ed1778d028394fdfc19d0a3d1dcc02b2960cf5c
Amar Zaidi <amarzaidi12321@gmail.com>
2023-11-23 22:31:35 +0500

    Delete CartItem.js
    Unused component is removed

--- Backend/models/CartItem.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const CartItemSchema = new mongoose.Schema({
-  userId: String,
-  productId: String, // Corrected typo: 'ProductId' to 'productId'
-  status: String,
-});
-
-module.exports = mongoose.model('CartItem', CartItemSchema);
