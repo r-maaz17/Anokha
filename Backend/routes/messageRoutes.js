ef5c64b7ce2eb3c23d7cb945a090a41de916f389
Amar Zaidi <amarzaidi12321@gmail.com>
2023-11-23 22:29:49 +0500

    Update messageRoutes.js
    Comments are added

--- Backend/routes/messageRoutes.js
+++ Backend/routes/messageRoutes.js
@@ -3,9 +3,14 @@ const express = require('express');
 const router = express.Router();
 const messageController = require('../controller/messageController');
 
- // Create a new customer
+// Get All messages from database
 router.get('/messages',messageController.getAllMessages);
+
+// Create new message
 router.post('/messages',messageController.CreateNewMessage);
+
+//set the Message / Issue status 
+router.put('/set-message-status/:id',messageController.setMessageStatus)
 
 module.exports = router;
 
