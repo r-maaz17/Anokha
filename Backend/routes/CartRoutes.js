661887ccbb49ace7c083a1e5ecec6b294c773116
Amar Zaidi <amarzaidi12321@gmail.com>
2023-11-23 22:29:38 +0500

    Update CartRoutes.js
    Comments are added

--- Backend/routes/CartRoutes.js
+++ Backend/routes/CartRoutes.js
@@ -3,9 +3,23 @@ const express = require('express');
 const router = express.Router();
 const CartController = require('../controller/cartController');
 
- // Create a new customer
-router.put('/cart', verifyTokenMiddleware,CartController.addItemIntoCart);
-router.delete('/cart',verifyTokenMiddleware,CartController.deleteFromCart)
-router.post('/cartitems', verifyTokenMiddleware,CartController.getCartItems);
+// edit cartItems in the cart
+router.put('/cart/:id', verifyTokenMiddleware,CartController.editItemIntoCart);
+
+// add cartItems in the cart
+router.post('/cart', verifyTokenMiddleware,CartController.addItemIntoCart);
+
+// delete cartItem of the cart
+router.delete('/cart/:id',verifyTokenMiddleware,CartController.deleteFromCart)
+
+// Delete the all cartItems
+router.put('/delete-cart/',verifyTokenMiddleware,CartController.emptyCart);
+
+//get all cartItems
+router.get('/cartitems', verifyTokenMiddleware,CartController.getCartItems);
+
+// get cartItem from cart of specific product Id
+router.get('/cartitem/:productId', verifyTokenMiddleware,CartController.getCartItem);
+
 module.exports = router;
 
