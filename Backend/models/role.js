f0d117161b6f5145400a85707a6f30b40394f701
Amar Zaidi <amarzaidi12321@gmail.com>
2023-11-25 12:12:57 +0500

    Delete Unused Files
    Some files are unused in project and needs to be removed

--- Backend/models/role.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const roleSchema = new mongoose.Schema({
-  RoleId: Number,
-  RoleName: String,
-});
-
-module.exports = mongoose.model('Role', roleSchema);
