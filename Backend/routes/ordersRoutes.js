614551a4d411d2c472edb426eb05c7304ffc8220
Amar Zaidi <amarzaidi12321@gmail.com>
2023-11-25 11:20:32 +0500

    Update ordersRoutes.js
    Middlewares are added to order routes

--- Backend/routes/ordersRoutes.js
+++ Backend/routes/ordersRoutes.js
@@ -1,21 +1,21 @@
 const express = require('express');
 const router = express.Router();
 const ordersController = require('../controller/ordersController');
-const { verifyTokenMiddleware }= require('../middleware/authMiddleware');
+const { verifyAdminMiddleware }= require('../middleware/authMiddleware');
 
 // Create a new order
-router.post('/order/create', verifyTokenMiddleware,ordersController.createOrder);
+router.post('/order/create', verifyAdminMiddleware,ordersController.createOrder);
 
 // Get all orders
-router.get('/orders', ordersController.getOrders);
+router.get('/orders', verifyAdminMiddleware,ordersController.getOrders);
 
 // Get an order by OrderId
-router.get('/orders/:orderId', ordersController.getOrderById);
+router.get('/orders/:orderId', verifyAdminMiddleware,ordersController.getOrderById);
 
 // Update an order by OrderId
-router.put('/order/:id',verifyTokenMiddleware,ordersController.setOrderStatus);
+router.put('/order/:id',verifyAdminMiddleware,ordersController.setOrderStatus);
 
 // Delete an order by OrderId
-router.delete('/orders/:orderId', ordersController.deleteOrder);
+router.delete('/orders/:orderId', verifyAdminMiddleware,ordersController.deleteOrder);
 
 module.exports = router;
