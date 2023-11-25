30627f1be040dc067800877194d1818ecf9a48ae
Amar Zaidi <amarzaidi12321@gmail.com>
2023-11-25 11:20:08 +0500

    Update Message.js
    Audit table is implemented

--- Backend/models/Message.js
+++ Backend/models/Message.js
@@ -7,7 +7,27 @@ const messageSchema = new mongoose.Schema({
   name: String,
   email: String,
   Message: String,
-  status:String
+  status:String,
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now },
+  createdBy: String,
+  updatedBy: String,
+});
+
+messageSchema.pre('save', function (next) {
+  // Update the updatedAt field and createdBy field before saving
+  this.updatedAt = new Date();
+  if (!this.createdBy) {
+    this.createdBy = this.updatedBy;
+  }
+  next();
+});
+
+messageSchema.pre('findOneAndUpdate', function (next) {
+  // Update the updatedAt field and updatedBy field before update
+  this._update.updatedAt = new Date();
+  this._update.updatedBy = this._conditions.createdBy || 'unknown';
+  next();
 });
 
 module.exports = mongoose.model('Message', messageSchema);
